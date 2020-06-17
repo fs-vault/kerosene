4e4226a3593bf977a215da24c9571600706c13bd
Mykyta <nkomarn@hotmail.com>
2020-06-17 07:39:44 -0700

    🔨 ItemBuilder and SkullBuilder utilities These utilities allow quick and easy creation of ItemStacks in a builder format.

---
+++ src/main/java/xyz/nkomarn/Kerosene/util/item/SkullBuilder.java
@@ -0,0 +1,44 @@
+package xyz.nkomarn.Kerosene.util.item;
+
+import com.mojang.authlib.GameProfile;
+import com.mojang.authlib.properties.Property;
+import org.bukkit.Material;
+import org.bukkit.OfflinePlayer;
+import org.bukkit.inventory.meta.SkullMeta;
+
+import java.lang.reflect.Field;
+import java.util.UUID;
+
+/**
+ * Utility class intended to allow fast building of Skulls.
+ */
+public class SkullBuilder extends ItemBuilder {
+
+    public SkullBuilder() {
+        super(Material.PLAYER_HEAD);
+    }
+
+    public SkullBuilder player(OfflinePlayer player) {
+        SkullMeta meta = (SkullMeta) this.item.getItemMeta();
+        meta.setOwningPlayer(player);
+        this.item.setItemMeta(meta);
+        return this;
+    }
+
+    public SkullBuilder texture(String texture) {
+        SkullMeta meta = (SkullMeta) this.item.getItemMeta();
+        GameProfile profile = new GameProfile(UUID.randomUUID(), null);
+        profile.getProperties().put("textures", new Property("textures", texture));
+
+        try {
+            Field field = meta.getClass().getDeclaredField("profile");
+            field.setAccessible(true);
+            field.set(meta, profile);
+        } catch (NoSuchFieldException | IllegalAccessException e) {
+            e.printStackTrace();
+        }
+
+        this.item.setItemMeta(meta);
+        return this;
+    }
+}
