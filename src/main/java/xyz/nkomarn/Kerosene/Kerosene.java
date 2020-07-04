14044cc207dd89aad8514a2b73b93554ff5065bc
Jeroen Verwimp <jeroen@verwimp.me>
2020-07-04 13:08:07 +0200

    🗑️ Removed test command

--- src/main/java/xyz/nkomarn/Kerosene/Kerosene.java
+++ src/main/java/xyz/nkomarn/Kerosene/Kerosene.java
@@ -71,7 +71,6 @@ public class Kerosene extends JavaPlugin {
         Bukkit.getPluginManager().registerEvents(new PlayerQuitListener(), this);
 
         getCommand("kerosene").setExecutor(new KeroseneCommand());
-        getCommand("test").setExecutor(this); // TODO remove
     }
 
     @Override
@@ -79,35 +78,6 @@ public class Kerosene extends JavaPlugin {
         MenuHandler.closeAll();
         PlayerData.close();
         Redis.close();
-    }
-
-    private static boolean state = true; // TODO remove
-    private static boolean state2 = true; // TODO remove
-
-    @Override
-    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) { // TODO remove
-        Gui gui = new Gui("Test", 4);
-        gui.addElement(new BorderComponent());
-
-        List<String> details = new ArrayList<>();
-        details.add("&7Makes every armor stand");
-        details.add("&7you place have arms.");
-
-        List<String> details2 = new ArrayList<>();
-        details2.add("&7Enabled your PvP and allows");
-        details2.add("&7you to fight with other players.");
-
-        gui.addElement(new ToggleButtonComponent(new GuiPosition(1, 1), "Toggle: Armor Stand Arms", details, Material.ARMOR_STAND, Material.ARMOR_STAND, () -> state, event -> {
-            state = event.getNewState();
-        }));
-
-        gui.addElement(new ToggleButtonComponent(new GuiPosition(2, 1), "PvP", details2, Material.IRON_AXE, Material.SHIELD, () -> state2, event -> {
-            state2 = event.getNewState();
-        }));
-
-        gui.open((Player) sender);
-
-        return super.onCommand(sender, command, label, args);
     }
 
     /**
