d708e98feaaeedca82e17ce04f2d6a0b072a6360
Jeroen Verwimp <jeroen@verwimp.me>
2020-08-07 00:45:35 +0200

    Remove closing from navigateToParent to allow multi user gui's

--- src/main/java/xyz/nkomarn/kerosene/gui/predefined/ConfirmationGui.java
+++ src/main/java/xyz/nkomarn/kerosene/gui/predefined/ConfirmationGui.java
@@ -82,7 +82,6 @@ public class ConfirmationGui extends Gui {
             this.cancelCallback = event -> { // default
                 GuiDefaults.playSelectSound(event.getPlayer());
                 event.getGui().navigateToParent();
-                event.getGui().close();
             };
         }
 
