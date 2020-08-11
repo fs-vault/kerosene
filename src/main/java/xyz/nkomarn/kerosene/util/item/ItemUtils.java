61d2b32326d6ea3c648d366e11f442b3ee16de7c
Mykyta Komarn <nkomarn@hotmail.com>
2020-08-10 21:18:06 -0700

    Add more utils.

--- src/main/java/xyz/nkomarn/kerosene/util/item/ItemUtils.java
+++ src/main/java/xyz/nkomarn/kerosene/util/item/ItemUtils.java
@@ -1,7 +1,9 @@
 package xyz.nkomarn.kerosene.util.item;
 
 import org.bukkit.Material;
+import org.bukkit.craftbukkit.v1_16_R1.inventory.CraftItemStack;
 import org.bukkit.inventory.ItemStack;
+import org.jetbrains.annotations.NotNull;
 
 public final class ItemUtils {
 
@@ -15,4 +17,7 @@ public final class ItemUtils {
                 .build();
     }
 
+    public static @NotNull String getFriendlyName(Material material) {
+        return CraftItemStack.asNMSCopy(new ItemStack(material)).getName().getString();
+    }
 }
--- src/main/java/xyz/nkomarn/kerosene/util/player/PlayerUtil.java
+++ src/main/java/xyz/nkomarn/kerosene/util/player/PlayerUtil.java
@@ -3,6 +3,8 @@ package xyz.nkomarn.kerosene.util.player;
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.util.Vector;
+import org.jetbrains.annotations.NotNull;
 import xyz.nkomarn.kerosene.Kerosene;
 
 public final class PlayerUtil {
@@ -22,6 +24,23 @@ public final class PlayerUtil {
                 player.getWorld().dropItemNaturally(player.getLocation(), overflowStack);
             }));
         });
+    }
+
+    /**
+     * Adds velocity to a player, effectively making them get knocked back from their current location.
+     * @param player The player to apply the velocity vector to.
+     * @param multiplier Velocity multiplier.
+     * @param y Additional y-axis velocity.
+     */
+    public static void knockback(@NotNull Player player, int multiplier, int y) {
+        Vector locationVector = player.getLocation().toVector();
+        Vector vector = locationVector.normalize().multiply(multiplier).setY(y);
+
+        if (player.isInsideVehicle()) {
+            player.getVehicle().setVelocity(vector);
+        } else {
+            player.setVelocity(vector);
+        }
     }
 
 }
