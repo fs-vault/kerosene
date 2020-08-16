351526c15f6912c66c7dce54ac1e72001dc5b1c7
Mykyta Komarn <nkomarn@hotmail.com>
2020-08-16 10:07:02 -0700

    Remove Redis connection pooling + upgrade Hikari

--- src/main/java/com/firestartermc/kerosene/data/redis/Redis.java
+++ src/main/java/com/firestartermc/kerosene/data/redis/Redis.java
@@ -8,13 +8,8 @@ import io.lettuce.core.api.async.RedisAsyncCommands;
 import io.lettuce.core.api.reactive.RedisReactiveCommands;
 import io.lettuce.core.api.sync.RedisCommands;
 import io.lettuce.core.pubsub.api.async.RedisPubSubAsyncCommands;
-import io.lettuce.core.support.ConnectionPoolSupport;
-import org.apache.commons.pool2.impl.GenericObjectPool;
-import org.apache.commons.pool2.impl.GenericObjectPoolConfig;
 import org.jetbrains.annotations.NotNull;
 import reactor.core.publisher.Mono;
-
-import java.util.concurrent.CompletableFuture;
 
 /**
  * Database class which allows for easy pooled access to Redis database connections using Jedis.
@@ -22,36 +17,28 @@ import java.util.concurrent.CompletableFuture;
 public class Redis {
 
     private final RedisClient client;
-    private final GenericObjectPool<StatefulRedisConnection<String, String>> pool;
+    private final StatefulRedisConnection<String, String> connection;
     private final RedisPubSubAsyncCommands<String, String> pubSub;
 
     public Redis(@NotNull String uri) {
         RedisURI redisUri = RedisURI.create(uri);
 
         this.client = RedisClient.create(redisUri);
-        this.pool = ConnectionPoolSupport.createGenericObjectPool(client::connect, new GenericObjectPoolConfig());
+        this.connection = client.connect();
         this.pubSub = client.connectPubSub().async();
         this.pubSub.getStatefulConnection().addListener(new PubSubHandler());
     }
 
-    private @NotNull StatefulRedisConnection<String, String> borrow() {
-        try {
-            return pool.borrowObject();
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
-    }
-
     public @NotNull RedisCommands<String, String> sync() {
-        return borrow().sync();
+        return connection.sync();
     }
 
     public @NotNull RedisAsyncCommands<String, String> async() {
-        return borrow().async();
+        return connection.async();
     }
 
     public @NotNull RedisReactiveCommands<String, String> reactive() {
-        return borrow().reactive();
+        return connection.reactive();
     }
 
     public void subscribe(@NotNull String... channels) {
@@ -68,10 +55,10 @@ public class Redis {
     }
 
     public void shutdown() {
-        if (pool == null) {
+        if (connection == null) {
             return;
         }
 
-        pool.close();
+        connection.close();
     }
 }
