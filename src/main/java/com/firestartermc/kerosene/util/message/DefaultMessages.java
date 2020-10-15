95f897facda04c8313532beb461ec7b84bd1508c
Mykyta Komarnytskyy <nkomarn@hotmail.com>
2020-10-15 09:30:08 -0700

    Default messages utility

---
+++ src/main/java/com/firestartermc/kerosene/util/message/DefaultMessages.java
@@ -0,0 +1,12 @@
+package com.firestartermc.kerosene.util.message;
+
+import org.bukkit.ChatColor;
+
+public class DefaultMessages {
+
+    private DefaultMessages() {
+    }
+
+    public static final String FAILED_TO_LOAD_DATA = ChatColor.RED + "Failed to read your user data. Notify an admin.";
+
+}
