55120d582cb9f45ae2fd26135bec8848055c0654
Mykyta Komarnytskyy <nkomarn@hotmail.com>
2021-05-08 15:15:56 -0700

    visible players default completion

--- src/main/java/com/firestartermc/kerosene/command/CommandManager.java
+++ src/main/java/com/firestartermc/kerosene/command/CommandManager.java
@@ -4,7 +4,11 @@ import co.aikar.commands.BukkitCommandCompletionContext;
 import co.aikar.commands.CommandCompletions;
 import co.aikar.commands.PaperCommandManager;
 import com.firestartermc.kerosene.Kerosene;
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
 import org.jetbrains.annotations.NotNull;
+
+import java.util.stream.Collectors;
 
 public class CommandManager {
 
@@ -13,6 +17,19 @@ public class CommandManager {
     public CommandManager(@NotNull Kerosene kerosene) {
         commandManager = new PaperCommandManager(kerosene);
         commandManager.enableUnstableAPI("brigadier");
+
+        registerCompletion("visible_players", context -> {
+            if (context.getSender() instanceof Player) {
+                return Bukkit.getOnlinePlayers().stream()
+                        .filter(player -> context.getPlayer().canSee(player))
+                        .map(Player::getName)
+                        .collect(Collectors.toList());
+            }
+
+            return Bukkit.getOnlinePlayers().stream()
+                    .map(Player::getName)
+                    .collect(Collectors.toList());
+        });
     }
 
     @NotNull
