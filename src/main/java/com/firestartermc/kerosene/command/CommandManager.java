d8690400915fba72924df4c5b704938b8708d674
Mykyta Komarnytskyy <nkomarn@hotmail.com>
2021-05-12 16:03:15 -0700

    unregister registered commands on plugin disable

--- src/main/java/com/firestartermc/kerosene/command/CommandManager.java
+++ src/main/java/com/firestartermc/kerosene/command/CommandManager.java
@@ -4,20 +4,30 @@ import co.aikar.commands.BukkitCommandCompletionContext;
 import co.aikar.commands.CommandCompletions;
 import co.aikar.commands.PaperCommandManager;
 import com.firestartermc.kerosene.Kerosene;
+import com.google.common.collect.ArrayListMultimap;
+import com.google.common.collect.Multimap;
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.server.PluginDisableEvent;
+import org.bukkit.plugin.Plugin;
+import org.bukkit.plugin.java.JavaPlugin;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.stream.Collectors;
 
-public class CommandManager {
+public class CommandManager implements Listener {
 
     private final PaperCommandManager commandManager;
+    private final Multimap<Plugin, Command> registeredCommands;
 
     public CommandManager(@NotNull Kerosene kerosene) {
         commandManager = new PaperCommandManager(kerosene);
         commandManager.enableUnstableAPI("brigadier");
+        registeredCommands = ArrayListMultimap.create();
 
+        kerosene.getServer().getPluginManager().registerEvents(this, kerosene);
         registerCompletion("visible_players", context -> {
             if (context.getSender() instanceof Player) {
                 return Bukkit.getOnlinePlayers().stream()
@@ -40,6 +50,7 @@ public class CommandManager {
     public void registerCommands(@NotNull Command... commands) {
         for (var command : commands) {
             getRawManager().registerCommand(command, true);
+            registeredCommands.put(JavaPlugin.getProvidingPlugin(command.getClass()), command);
         }
     }
 
@@ -51,5 +62,10 @@ public class CommandManager {
 
     public void registerCompletion(@NotNull String id, @NotNull CommandCompletions.AsyncCommandCompletionHandler<BukkitCommandCompletionContext> handler) {
         getRawManager().getCommandCompletions().registerCompletion(id, handler);
+    }
+
+    @EventHandler
+    public void onUnload(PluginDisableEvent event) {
+        registeredCommands.get(event.getPlugin()).forEach(this::unregisterCommands);
     }
 }
